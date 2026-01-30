49db57d20aab0c6c428712c62f2dd3779d33d736
Hugo Nascimento <159330874+hugosnascimento@users.noreply.github.com>
2026-01-30 12:08:55 -0300

    update

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { PeopleOpsFlowCanvas } from './experiments/people-ops-orchestrator-v1/adapters/reactflow/PeopleOpsFlowCanvas'
+
+function App() {
+    return (
+        <div style={{ width: '100vw', height: '100vh' }}>
+            <PeopleOpsFlowCanvas />
+        </div>
+    )
+}
+
+export default App
